65d4e218a7ef8a265a009a03bf2378db25024e7a
gseok <ollehshode99@naver.com>
2025-04-11 09:32:23 +0900

    first commit

---
+++ src/test/java/com/gseok/JenkinscicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gseok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinscicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
